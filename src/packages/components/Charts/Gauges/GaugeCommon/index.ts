ea1feaab44c5659f7b9a3440e868a50c42ac6cf3
lixin <1453287107@qq.com>
2022-10-19 13:57:38 +0800

    feat: 新增仪表盘组件
    (cherry picked from commit 1d59cf641e540da5b2357fa316d78d1027a247df)

---
+++ src/packages/components/Charts/Gauges/GaugeCommon/index.ts
@@ -0,0 +1,15 @@
+import image from '@/assets/images/chart/charts/gauge.png'
+import { ConfigType, PackagesCategoryEnum, ChartFrameEnum } from '@/packages/index.d'
+import { ChatCategoryEnum, ChatCategoryEnumName } from '../../index.d'
+
+export const GaugeCommonConfig: ConfigType = {
+    key: 'GaugeCommon',
+    chartKey: 'VGaugeCommon',
+    conKey: 'VCGaugeCommon',
+    title: '仪表盘',
+    category: ChatCategoryEnum.GAUGE,
+    categoryName: ChatCategoryEnumName.GAUGE,
+    package: PackagesCategoryEnum.CHARTS,
+    chartFrame: ChartFrameEnum.ECHARTS,
+    image
+}
--- src/packages/components/Charts/index.ts
+++ src/packages/components/Charts/index.ts
@@ -4,5 +4,6 @@ import Lines from './Lines'
 import Scatters from './Scatters'
 import Mores from './Mores'
 import Maps from './Maps'
+import Gauges from  './Gauges'
 
-export const ChartList = [...Bars, ...Lines, ...Pies, ...Scatters, ...Maps, ...Mores]
+export const ChartList = [...Bars, ...Lines, ...Pies, ...Scatters, ...Gauges, ...Maps, ...Mores]
